9157b024bf667e5f91a301cb0df84bb0c6850e59
Carlos <carlosdaniellazo06@gmail.com>
2025-03-06 02:19:24 -0300

    notfound listo

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,8 +4,8 @@ import React from "react";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-4">404 - Página no encontrada</h1>
-      <p className="text-lg mb-8">
+      <h1 className="text-4xl text-center font-bold mb-4 px-1">404 - Página no encontrada</h1>
+      <p className="text-lg text-center mb-8 px-1">
         Lo sentimos, la página que buscas no existe.
       </p>
       <Link href="/" className="py-3 px-4 bg-blue-600 rounded-full hover:bg-blue-800">
